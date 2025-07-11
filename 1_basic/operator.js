f9e46c2c11de01b9dc144141bf44902083c0b2fb
gunrein <gunrein620@gmail.com>
2025-07-11 12:45:01 +0900

    NaN & 할당연산자의 이해

--- 1_basic/operator.js
+++ 1_basic/operator.js
@@ -13,3 +13,40 @@ result = ++number
 console.log(result, number);
 result = --number
 console.log(result, number);
+console.log('-'.repeat(40));
+
+let sample = '99'
+console.log(sample + 1); // 99 + 1 = 991 
+
+sample = true // 1 
+console.log(+sample);
+console.log(typeof sample); // boolean
+console.log(typeof +sample); // number
+
+
+sample = false // 0
+console.log(typeof sample); //boolean
+console.log(typeof +sample); // number
+console.log('-'.repeat(40));
+
+
+// NaN = not a number 
+sample = '강아지'
+console.log(+sample);
+console.log(-sample);
+
+sample = '99';
+console.log(+sample);
+console.log(-sample); 
+console.log('-'.repeat(40));
+
+// 할당연산자 assignment operator
+number = 100;
+console.log(number);
+
+number += 10;
+console.log(number);
+
+number -= 10;
+console.log(number);
+
