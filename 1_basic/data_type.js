df43c8f657062e51b7f8d66872718634ed48b79d
gunrein <gunrein620@gmail.com>
2025-07-11 11:16:03 +0900

    object 대한 이해

---
+++ 1_basic/data_type.js
@@ -0,0 +1,13 @@
+// object 
+
+const dictionary = {
+  // 키 : 밸류 
+  apple : '사과',
+  banana : '바나나',
+  greeting : 'hello'
+}
+
+console.log(dictionary); // 전체보여줨
+console.log(dictionary['apple']); // 키 -> 밸류 
+console.log(dictionary['바나나']); // 밸류 -> undefined
+console.log(typeof dictionary); // object 타입
