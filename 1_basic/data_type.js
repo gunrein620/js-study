a6eaf38c32f4533c5923532e438e99157f8daac2
gunrein <gunrein620@gmail.com>
2025-07-11 11:25:27 +0900

    array의 대한 이해

--- 1_basic/data_type.js
+++ 1_basic/data_type.js
@@ -1,4 +1,4 @@
-// object 
+// object 중괄호!!
 
 const dictionary = {
   // 키 : 밸류 
@@ -11,3 +11,24 @@ console.log(dictionary); // 전체보여줨
 console.log(dictionary['apple']); // 키 -> 밸류 
 console.log(dictionary['바나나']); // 밸류 -> undefined
 console.log(typeof dictionary); // object 타입
+console.log('-'.repeat(50)); // 구분선 
+
+// array 
+// 타입은 Object 
+// 값을 리스트로 나열 할 수 있는 타입 
+// object 중괄호 대비 array는 대괄호 
+
+const colorArray = [
+  'red', // index : 0
+  'orange', // index : 1 
+  'green',
+  'blue',
+  'black',
+  'cyan'
+]
+
+console.log(colorArray);
+
+// index = 0번 부터 시작 ~ 
+
+console.log(colorArray[1]);
