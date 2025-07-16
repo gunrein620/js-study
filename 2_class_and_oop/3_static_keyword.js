10e6dca39e1c1fe7342b4d4f56742c05e44b6e9c
gunrein <gunrein620@gmail.com>
2025-07-16 21:47:16 +0900

    factory construtor의 이해

--- 2_class_and_oop/3_static_keyword.js
+++ 2_class_and_oop/3_static_keyword.js
@@ -1,21 +1,56 @@
 // static 클래스에 귀속됨 
 
+// class Idolmodel{
+//   name;
+//   year;
+//   static groupName = 'IVE'
+
+//   constructor(name, year){
+//     this.name = name;
+//     this.year = year;
+//   }
+//   static returnGroupName(){
+//     return `아이브`;
+//   }
+// } 
+
+// const yuJin = new Idolmodel('안유진', 2003);
+// console.log(yuJin);  
+// console.log(Idolmodel.groupName);
+// console.log(Idolmodel.returnGroupName());
+
+// ** factory constructor 
 class Idolmodel{
   name;
   year;
-  static groupName = 'IVE'
-
-  constructor(name, year){
+  constructor(name,year){
     this.name = name;
-    this.year = year;
+    this.year = year
   }
-  static returnGroupName(){
-    return `아이브`;
+
+  static fromObject(object){
+      return new Idolmodel(
+        object.name,
+        object.year,
+      )
   }
-} 
 
-const yuJin = new Idolmodel('안유진', 2003);
-console.log(yuJin);  
-console.log(Idolmodel.groupName);
-console.log(Idolmodel.returnGroupName());
+  static fromList(list) {
+    return new Idolmodel(
+      list[0],
+      list[1],
+    )
+  }
+}
 
+const yuJin2 = Idolmodel.fromObject({
+  name: '안유진',
+  year:  2003,
+})
+
+console.log(yuJin2);
+
+const wonyooung = Idolmodel.fromList([
+  '장원영',2003
+])
+console.log(wonyooung);
