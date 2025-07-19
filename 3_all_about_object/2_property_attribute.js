da1921d99977cc98a7efb123749d9ff9b57f7d0f
gunrein <gunrein620@gmail.com>
2025-07-19 17:18:40 +0900

    writable의 이해

--- 3_all_about_object/2_property_attribute.js
+++ 3_all_about_object/2_property_attribute.js
@@ -19,6 +19,8 @@ console.log(Object.getOwnPropertyDescriptor(yuJin, 'year'));
  * 3. enumerable - 열거가 가능한지에 대한 여부이다. for...in 사용가능하면 true
  * 4. configurable - 프로퍼티 어트리뷰트의 재정의가 가능한지 여부를 판단한다. 
  *                   단, writable이 true인 경우 값 변경과 writable을 변경하는건 가능
+ * 
+ * 밸라이콘
  */
 console.log(Object.getOwnPropertyDescriptor(yuJin, 'name'));
 
@@ -57,3 +59,32 @@ console.log(Object.getOwnPropertyDescriptor(yuJin2, 'age'));
   configurable: true
 }
  */
+
+//프로퍼티 재정의
+// 프로퍼티를 추가하는 2가지 방법
+//1.  
+yuJin2.height = 172;
+//2.
+yuJin2['height'] = 172;
+console.log(yuJin2);
+
+Object.defineProperty(yuJin2, 'height',{
+  value: 173,
+  writable: true,
+  enumerable: true,
+  configurable: true,
+})
+
+console.log(yuJin2);
+console.log(Object.getOwnPropertyDescriptor(yuJin2,'height'));
+
+yuJin2.height = 180;
+console.log(yuJin2);
+
+Object.defineProperty(yuJin2, 'height',{
+  writable: false,
+});
+
+console.log('-'.repeat(45));
+yuJin2.height = 132
+console.log(Object.getOwnPropertyDescriptor(yuJin2,'height'));
